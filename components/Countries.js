27e2dffe2961b574561665e8a53c0c3198781a4d
Rasujon3 <rasujon3@gmail.com>
2022-05-28 23:41:18 +0600

    79-3 Display image in a Scrollview and implement search

--- components/Countries.js
+++ components/Countries.js
@@ -1,23 +1,48 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, TextInput, StyleSheet } from "react-native";
 import React, { useState, useEffect } from "react";
 import Country from "./Country";
 
 export default function Countries() {
   const [countries, setCountries] = useState([]);
+  const [searched, setSearched] = useState([]);
   useEffect(() => {
     const url = `https://restcountries.com/v3.1/all`;
     fetch(url)
       .then((res) => res.json())
-      .then((data) => setCountries(data));
+      .then((data) => {
+        setSearched(data);
+        setCountries(data);
+      });
   }, []);
+  const handleSearch = (text) => {
+    const filtered = countries.filter((country) =>
+      country.name.common.includes(text)
+    );
+    setSearched(filtered);
+  };
   return (
-    <View>
-      <Text>Visiting Countries: {countries.length}</Text>
-      <ScrollView>
-        {countries.map((country) => (
+    <ScrollView>
+      <View>
+        <Text style={styles.header}>Visiting Countries: {searched.length}</Text>
+        <TextInput onChangeText={handleSearch} style={styles.input}></TextInput>
+        {searched.map((country) => (
           <Country key={country.ccn3} country={country} />
         ))}
-      </ScrollView>
-    </View>
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    marginTop: 50,
+    fontSize: 40,
+    color: "red",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
